326d1b307d36beb07800b00c09ce4f4d11e69886
jdlambright <64182772+jdlambright@users.noreply.github.com>
2021-06-14 16:24:17 -0400

    work from our starter conversation

---
+++ step_one.py
@@ -0,0 +1,10 @@
+names = ("Michael", "James", "Oscar", "Fred", "Steve", "Ace", "Bill", "Marcus", "Brian", "Jeremy",
+         "Eliza", "Faith", "Sally", "Maria", "Jacqueline", "Cheri", "Ashley", "Emily", "Bianca", "Alice")
+gender = ("male", "female")
+eyes = ["blue", "green", "brown"]
+hair = ["black", "red", "blonde", "grey", "brown"]
+race = ["white", "black", "hispanic", "asian"]
+rosie = ["yes", "no"]
+facial = ["none", "mustache", "beard"]
+eye_lashes = ["long", "short"]
+jewelry = ["necklace", "ear rings", "glasses", "none"]
