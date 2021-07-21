000cbd69f054b8fdc81bb6d15da2c7dd1d6fcd9f
Tauseef-Hilal <tantary.tauseef.atwork@gmail.com>
2021-07-21 15:27:21 +0530

    Modified some files and added a new file --> src/display.py
    + Added src/display.py to display cards on screen.
    + Improvements in the card class.
    + Added some cards in Data/cards.json

--- src/classes.py
+++ src/classes.py
@@ -1,41 +1,32 @@
 """
 Classes.py
 """
-from random import choice
+# from random import choice
+
 
 class Card:
     """Defines a card"""
 
-    def __init__(self, 
-                 name,
-                 gender,
-                 eye_color,
-                 hair_color,
-                 skin_color,
-                 is_bald: bool,
-                 earrings: bool):
+    def __init__(self, details_dict: dict) -> None:
 
-        self.name = name
-        self.gender = gender
-        self.eye_color = eye_color
-        self.hair_color = hair_color
-        self.skin_color = skin_color
-        self.is_bald = is_bald
-        self.earrings = earrings
-    
-    def __repr__(self):
-        return f"Card >> {self.name}"
+        # Iterate over the keys of the dictionary and set instance attributes 
+        # with dict-keys as their names and dict-values as their values
+        for key, value in details_dict.items():
+            self.__setattr__(key, value)
+
+    def __repr__(self) -> str:
+        return f"Card({self.name!r}, {self.gender!r})"
 
 
-class Player:
-    """Model a player"""
+# class Player:
+#     """Model a player"""
 
-    def __init__(self, cards, is_human=True) -> None:
-        self.cards = cards
+#     def __init__(self, cards, is_human=True) -> None:
+#         self.cards = cards
 
-        if not is_human:
-            self.secret_card = choice(self.cards)
-        else:
-            # Call a function to get human player's input
-            pass
+#         if not is_human:
+#             self.secret_card = choice(self.cards)
+#         else:
+#             # Call a function to get human player's input
+#             pass
     
